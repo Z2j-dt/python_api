e621b25c74f440a501325fec0a1f8061ce0bb3fd
Z2j-dt <752599853@qq.com>
2026-04-13 08:59:58 +0800

    其他（scrm）

--- daliy/mv_tag_unionid_permission.sql
+++ daliy/mv_tag_unionid_permission.sql
@@ -100,3 +100,106 @@ FROM (
 WHERE tmp.tag_name IS NOT NULL
 GROUP BY tmp.tag_name
 ORDER BY tmp.tag_name ASC;
+
+
+CREATE MATERIALIZED VIEW `mv_tag_unionid_permission_array` (
+    `tag_name`,
+    `union_id_array`
+)
+COMMENT "客户标签union_id数组"
+DISTRIBUTED BY RANDOM
+REFRESH ASYNC START("2026-03-31 06:00:00") EVERY(INTERVAL 1 DAY)
+PROPERTIES (
+    "replicated_storage" = "true",
+    "replication_num" = "3",
+    "storage_medium" = "HDD"
+)
+AS
+SELECT
+    tmp.tag_name,
+    ARRAY_AGG(DISTINCT tmp.union_id) AS union_id_array
+FROM (
+    -- 重复添加
+    SELECT union_id, '重复添加' AS tag_name
+    FROM etl_db.tag_wxcust_profit
+    WHERE empy_cnts > 1
+
+    UNION ALL
+    -- 高频互动
+    SELECT union_id, '高频互动' AS tag_name
+    FROM etl_db.tag_wxcust_profit
+    WHERE `30d_wc_interact_cnt` >= 5
+
+    UNION ALL
+    -- 偶尔互动
+    SELECT union_id, '偶尔互动' AS tag_name
+    FROM etl_db.tag_wxcust_profit
+    WHERE `30d_wc_interact_cnt` >= 1
+      AND `30d_wc_interact_cnt` <= 4
+
+    UNION ALL
+    -- 长期不互动
+    SELECT union_id, '长期不互动' AS tag_name
+    FROM etl_db.tag_wxcust_profit
+    WHERE `30d_wc_interact_cnt` = 0
+
+    UNION ALL
+    -- 已开口
+    SELECT union_id, '已开口' AS tag_name
+    FROM etl_db.tag_wxcust_profit
+    WHERE is_spoken = '1'
+
+    UNION ALL
+    -- 高频观看（王柱）
+    SELECT union_id, '高频观看（王柱）' AS tag_name
+    FROM etl_db.tag_wxcust_profit
+    WHERE `30d_wangzhu_streams` >= 6
+
+    UNION ALL
+    -- 中频观看（王柱）
+    SELECT union_id, '中频观看（王柱）' AS tag_name
+    FROM etl_db.tag_wxcust_profit
+    WHERE `30d_wangzhu_streams` >= 2
+      AND `30d_wangzhu_streams` <= 5
+
+    UNION ALL
+    -- 低频观看（王柱）
+    SELECT union_id, '低频观看（王柱）' AS tag_name
+    FROM etl_db.tag_wxcust_profit
+    WHERE `30d_wangzhu_streams` = 1
+
+    UNION ALL
+    -- 高频观看（王雨时）
+    SELECT union_id, '高频观看（王雨时）' AS tag_name
+    FROM etl_db.tag_wxcust_profit
+    WHERE `30d_wangyushi_streams` >= 6
+
+    UNION ALL
+    -- 中频观看（王雨时）
+    SELECT union_id, '中频观看（王雨时）' AS tag_name
+    FROM etl_db.tag_wxcust_profit
+    WHERE `30d_wangyushi_streams` >= 2
+      AND `30d_wangyushi_streams` <= 5
+
+    UNION ALL
+    -- 低频观看（王雨时）
+    SELECT union_id, '低频观看（王雨时）' AS tag_name
+    FROM etl_db.tag_wxcust_profit
+    WHERE `30d_wangyushi_streams` = 1
+
+    UNION ALL
+    -- 直播商品点击（王柱）
+    SELECT union_id, '直播商品点击（王柱）' AS tag_name
+    FROM etl_db.tag_wxcust_profit
+    WHERE `30d_wangzhu_click_prod` = '1'
+
+    UNION ALL
+    -- 直播商品点击（王雨时）
+    SELECT union_id, '直播商品点击（王雨时）' AS tag_name
+    FROM etl_db.tag_wxcust_profit
+    WHERE `30d_wangyushi_click_prod` = '1'
+) tmp
+WHERE tmp.tag_name IS NOT NULL
+  AND tmp.union_id IS NOT NULL
+GROUP BY tmp.tag_name
+ORDER BY tmp.tag_name ASC;
